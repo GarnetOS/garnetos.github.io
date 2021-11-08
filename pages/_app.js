f8dc8df9f89a22786b632c983165bd5848cd6d2d
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-11-08 11:52:36 +0100

    Main page heading

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/index.scss";
+import "../styles/styles.scss";
 
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
+import Link from "next/link";
 import Header from "../components/Header";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 
 export default function Home() {
   const { t, i18n } = useTranslation();
@@ -15,7 +16,12 @@ export default function Home() {
       <main>
         <Header />
         <div className="container">
-          <h1>{t("helloworld")}</h1>
+          <h1>
+            <Trans i18nKey="index_h1" components={[<strong key="0" />]} />
+          </h1>
+          <Link href="/download">
+            <a className="btn btn-l btn-duotone">{t("index_cta")}</a>
+          </Link>
         </div>
       </main>
     </>
