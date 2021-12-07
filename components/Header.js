d519f75bdc152eef24fbb5d5b65f7bcf226ea757
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-12-07 13:07:21 +0100

    Responsive navbar

--- components/Header.js
+++ components/Header.js
@@ -1,35 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import LanguageSwitcher from "./LanguageSwitcher";
 import { useTranslation } from "react-i18next";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   const { t, i18n } = useTranslation();
+
+  const [navHidden, setNavHidden] = useState(true);
 
   return (
     <header className="py-8 bg-gray-900">
       <div className="px-4 container max-w-screen-xl mx-auto flex justify-between">
         <div className="flex items-center justify-start">
           <img src="/assets/logo.svg" alt="Logo" className="h-12" />
-          <nav className="ml-2 flex items-center justify-start">
+        </div>
+        <div className={`${navHidden ? "hidden" : "flex"} flex-col md:flex-row items-center md:justify-end md:flex absolute md:static z-10 bg-gray-900 top-24 left-0 right-0 pb-12 md:pb-0 border-b border-gray-500 md:border-none`}>
+          <nav className="mr-4 flex items-center justify-start flex-col md:flex-row">
             <Link href="/">
-              <a className="ml-3 font-semibold text-gray-300 transition duration-200 hover:text-gray-400">{t("header_home")}</a>
+              <a className="ml-3 mb-3 md:mb-0 font-semibold text-gray-300 transition duration-200 hover:text-gray-400">{t("header_home")}</a>
             </Link>
             <Link href="/documentation">
-              <a className="ml-3 font-semibold text-gray-300 transition duration-200 hover:text-gray-400">{t("header_documentation")}</a>
+              <a className="ml-3 mb-3 md:mb-0 font-semibold text-gray-300 transition duration-200 hover:text-gray-400">{t("header_documentation")}</a>
             </Link>
             <Link href="/support">
-              <a className="ml-3 font-semibold text-gray-300 transition duration-200 hover:text-gray-400">{t("header_support")}</a>
+              <a className="ml-3 mb-3 md:mb-0 font-semibold text-gray-300 transition duration-200 hover:text-gray-400">{t("header_support")}</a>
             </Link>
           </nav>
-        </div>
-        <div className="flex items-center justify-end">
           <Link href="/download">
-            <a className="btn btn-s btn-primary">{t("header_download")}</a>
+            <a className="btn btn-s btn-primary mb-2 md:mb-0  ">{t("header_download")}</a>
           </Link>
           <LanguageSwitcher short={true} />
         </div>
+        <div className="text-white text-3xl my-auto cursor-pointer md:hidden" onClick={() => setNavHidden(!navHidden)}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
       </div>
+      <div></div>
     </header>
   );
 }
